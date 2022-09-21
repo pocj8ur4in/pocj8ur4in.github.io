1eda2bb7a05cbfe33b5429bdb7781079e460bb67
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-21 12:29:00 +0900

    Post 2022-09-20-html1

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ gem "jekyll", "~> 3.5"
 gem "minimal-mistakes-jekyll"
 gem "kramdown-parser-gfm"
 gem "webrick", "~> 1.7"
-
 gem 'jekyll-sitemap'
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[HTML] 1. HTML 기초 정리",
+        "excerpt":"웹 (Web) 월드 와이드 웹 (WWW)이라고도 부름 인터넷을 통해 제공되는 전세계적인 정보 공유 서비스 공간 인터넷 (Internet) 전세계적으로 연결된 컴퓨터 네트워크 공간 수많은 클라이언트, 서버, 이들로 구성된 네트워크의 집합 TCP/IP 통신 규약을 통해 파일 송수신 네트워크 (Network) 컴퓨터들이 통신망을 통해 하나로 연결된 통신 체계 클라이언트 (Client) 네트워크에 연결되어 서버에 정보...","categories": ["HTML"],
+        "tags": ["HTML","WEB"],
+        "url": "/html/html1/",
+        "teaser": null
+      }]
