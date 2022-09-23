0cb2210c378f4cc79253554c5c82263034bcc170
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-23 13:50:03 +0900

    Post 2022-09-23-baekjoon2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,25 @@
 var store = [{
         "title": "[HTML] 1. HTML 기초",
-        "excerpt":"웹 (Web) 월드 와이드 웹 (WWW)이라고도 부름 인터넷을 통해 제공되는 전세계적인 정보 공유 서비스 공간 인터넷 (Internet) 전세계적으로 연결된 컴퓨터 네트워크 공간 수많은 클라이언트, 서버, 이들로 구성된 네트워크의 집합 TCP/IP 통신 규약을 통해 파일 송수신 네트워크 (Network) 컴퓨터들이 통신망을 통해 하나로 연결된 통신 체계 클라이언트 (Client) 네트워크에 연결되어 서버에 정보...","categories": ["HTML"],
-        "tags": ["FrontEnd"],
-        "url": "/html/html1/",
+        "excerpt":"웹 (Web) 월드 와이드 웹 (WWW)이라고도 부름 인터넷을 통해 제공되는 전세계적인 정보 공유 서비스 공간 인터넷 (Internet) 전세계적으로 연결된 컴퓨터 네트워크 공간 수많은 클라이언트, 서버, 이들로 구성된 네트워크의 집합 TCP/IP 통신 규약을 통해 파일 송수신 네트워크 (Network) 컴퓨터들이 통신망을 통해 하나로 연결된 통신 체계 클라이언트 (Client) 네트워크에 연결되어 서버에 정보...","categories": ["Frontend"],
+        "tags": [],
+        "url": "/frontend/html1/",
         "teaser": null
       },{
         "title": "[HTML] 2. HTML 태그 - <head>",
-        "excerpt":"&lt;html&gt; : HTML으로 작성된 문서임을 선언하는 태그 &lt;html&gt; ... &lt;/html&gt; &lt;!DOCTYPE html&gt; : 마크업 언어에서의 문서 형식을 정의하는 태그 DTD 태그 (Document Type Definition) 웹 브라우저의 렌더링 엔진이 관용 모드 (Quirks Mode)가 아닌 표준 모드로 렌더링 HTML5 :&lt;!DOCTYPE html&gt;으로 선언 &lt;!DOCTYPE html&gt; ... &lt;/html&gt; &lt;html lang=\"\"&gt; : HTML 문서의 텍스트가...","categories": ["HTML"],
-        "tags": ["FrontEnd"],
-        "url": "/html/html2/",
+        "excerpt":"&lt;html&gt; : HTML으로 작성된 문서임을 선언하는 태그 &lt;html&gt; ... &lt;/html&gt; &lt;!DOCTYPE html&gt; : 마크업 언어에서의 문서 형식을 정의하는 태그 DTD 태그 (Document Type Definition) 웹 브라우저의 렌더링 엔진이 관용 모드 (Quirks Mode)가 아닌 표준 모드로 렌더링 HTML5 :&lt;!DOCTYPE html&gt;으로 선언 &lt;!DOCTYPE html&gt; ... &lt;/html&gt; &lt;html lang=\"\"&gt; : HTML 문서의 텍스트가...","categories": ["Frontend"],
+        "tags": [],
+        "url": "/frontend/html2/",
         "teaser": null
       },{
         "title": "[BOJ] 1000. A+B (B5)",
-        "excerpt":"1. 문제 풀이   #include &lt;iostream&gt; using namespace std;  int main() {     ios_base::sync_with_stdio(false);     cin.tie(NULL);     cout.tie(NULL);          int A, B;          cin &gt;&gt; A &gt;&gt; B;          cout &lt;&lt; A + B;          return 0; }      &lt;iostream&gt; 클래스의 cin와 cout로 입출력 구현   2. 문제 분류     구현   수학 &gt; 사칙연산      주소 : https://www.acmicpc.net/problem/1000   ","categories": ["BOJ"],
-        "tags": ["C++","Coding Test"],
-        "url": "/boj/baekjoon1/",
+        "excerpt":"1. 문제 풀이   #include &lt;iostream&gt; using namespace std;  int main() {     ios_base::sync_with_stdio(false);     cin.tie(NULL);     cout.tie(NULL);          int A, B;          cin &gt;&gt; A &gt;&gt; B;          cout &lt;&lt; A + B;          return 0; }      &lt;iostream&gt; 클래스의 cin와 cout로 입출력 구현   2. 문제 분류     구현   수학 &gt; 사칙연산      주소 : https://www.acmicpc.net/problem/1000   ","categories": ["Baekjoon"],
+        "tags": [],
+        "url": "/baekjoon/baekjoon1/",
+        "teaser": null
+      },{
+        "title": "[BOJ] 1001. A-B (B5)",
+        "excerpt":"1. 문제 풀이   #include &lt;iostream&gt; using namespace std;  int main() {     ios_base::sync_with_stdio(false);     cin.tie(NULL);     cout.tie(NULL);          int A, B;          cin &gt;&gt; A &gt;&gt; B;          cout &lt;&lt; A - B;          return 0; }      &lt;iostream&gt; 클래스의 cin와 cout로 입출력 구현   2. 문제 분류     구현   수학 &gt; 사칙연산      주소 : https://www.acmicpc.net/problem/1001   ","categories": ["Baekjoon"],
+        "tags": [],
+        "url": "/baekjoon/baekjoon2/",
         "teaser": null
       }]
