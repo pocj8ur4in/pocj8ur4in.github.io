279b281f855e69153a0be7e39bd174083e8120e8
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-24 20:31:34 +0900

    Update 2022-09-23-baekjoon7.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -48,7 +48,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[BOJ] 1330. 두 수 비교하기 (B5)",
-        "excerpt":"1. 문제 조건 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다. A와 B가...","categories": ["Baekjoon"],
+        "excerpt":"1. 문제 조건 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 출력 : 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다....","categories": ["Baekjoon"],
         "tags": ["Python"],
         "url": "/baekjoon/baekjoon7/",
         "teaser": null
