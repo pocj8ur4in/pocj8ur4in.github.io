c7fde044c71332ef2d8fccb15894c965cfc46f43
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-24 19:47:45 +0900

    Update 2022-09-23-baekjoon7.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,50 +6,50 @@ var store = [{
         "teaser": null
       },{
         "title": "[HTML] 2. HTML 태그 - <head>",
-        "excerpt":"&lt;html&gt; : HTML으로 작성된 문서임을 선언하는 태그 &lt;html&gt; ... &lt;/html&gt; &lt;!DOCTYPE html&gt; : 마크업 언어에서의 문서 형식을 정의하는 태그 DTD 태그 (Document Type Definition) 웹 브라우저의 렌더링 엔진이 관용 모드 (Quirks Mode)가 아닌 표준 모드로 렌더링 HTML5 :&lt;!DOCTYPE html&gt;으로 선언 &lt;!DOCTYPE html&gt; ... &lt;/html&gt; &lt;html lang=\"\"&gt; : HTML 문서의 텍스트가...","categories": ["Frontend"],
+        "excerpt":"&lt;html&gt; : HTML으로 작성된 문서임을 선언하는 태그 &lt;html&gt; ... &lt;/html&gt; &lt;!DOCTYPE html&gt; : 마크업 언어에서의 문서 형식을 정의하는 태그 DTD 태그 (Document Type Definition) 웹 브라우저의 렌더링 엔진이 관용 모드 (Quirks Mode)가 아닌 표준 모드로 렌더링 HTML5 :&lt;!DOCTYPE html&gt;으로 선언 &lt;!DOCTYPE html&gt; ... &lt;/html&gt; &lt;html lang=\"\"&gt; : 웹 문서의 텍스트가...","categories": ["Frontend"],
         "tags": ["HTML"],
         "url": "/frontend/html2/",
         "teaser": null
       },{
         "title": "[BOJ] 1000. A+B (B5)",
-        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A+B를 출력한다. 2. 문제 풀이 #include &lt;iostream&gt; using namespace std; int main() { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); int A, B; cin...","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A+B를 출력한다. 2. 문제 풀이 a, b = input().split() print(int(a) + int(b)) 3. 문제 분류 구현 수학 &gt; 사칙연산 주소...","categories": ["Baekjoon"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon1/",
         "teaser": null
       },{
         "title": "[BOJ] 1001. A-B (B5)",
-        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A-B를 출력한다. 2. 문제 풀이 #include &lt;iostream&gt; using namespace std; int main() { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); int A, B; cin...","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A-B를 출력한다. 2. 문제 풀이 a, b = input().split() print(int(a) - int(b)) 2. 문제 분류 구현 수학 &gt; 사칙연산 주소...","categories": ["Baekjoon"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon2/",
         "teaser": null
       },{
         "title": "[BOJ] 2557. Hello World (B5)",
-        "excerpt":"1. 문제 조건  Hello World!를 출력하시오.     입력 : 없음   출력 : Hello World!   2. 문제 풀이   #include &lt;iostream&gt; using namespace std;  int main(){     ios_base::sync_with_stdio(false);     cin.tie(NULL);     cout.tie(NULL);          cout &lt;&lt; \"Hello World!\";      return 0; }      &lt;iostream&gt; 클래스의 cout로 입력 구현   3. 문제 분류     구현      주소 : https://www.acmicpc.net/problem/2557   ","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "excerpt":"1. 문제 조건  Hello World!를 출력하시오.     입력 : 없음   출력 : Hello World!   2. 문제 풀이   print(\"Hello World!\")   3. 문제 분류     구현      주소 : https://www.acmicpc.net/problem/2557   ","categories": ["Baekjoon"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon3/",
         "teaser": null
       },{
         "title": "[BOJ] 10998. A×B (B5)",
-        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A×B를 출력한다. 2. 문제 풀이 #include &lt;iostream&gt; using namespace std; int main(){ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); int A, B; cin &gt;&gt;...","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "excerpt":"1. 문제 조건 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 : 첫째 줄에 A×B를 출력한다. 2. 문제 풀이 a, b = input().split() print(int(a) * int(b)) 3. 문제 분류 구현 수학 &gt; 사칙연산 주소...","categories": ["Baekjoon"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon4/",
         "teaser": null
       },{
         "title": "[BOJ] 2588. 곱셈 (B3)",
         "excerpt":"1. 문제 조건 (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다. (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오. 입력 : 첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다....","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon5/",
         "teaser": null
       },{
         "title": "[BOJ] 10869. 사칙연산 (B5)",
-        "excerpt":"1. 문제 조건 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 입력 : 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000) 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다. 2. 문제 풀이 #include...","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "excerpt":"1. 문제 조건 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 입력 : 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000) 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다. 2. 문제 풀이 a,...","categories": ["Baekjoon"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon6/",
         "teaser": null
       },{
         "title": "[BOJ] 1330. 두 수 비교하기 (B5)",
         "excerpt":"1. 문제 조건 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 : 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다. A와 B가...","categories": ["Baekjoon"],
-        "tags": ["C++"],
+        "tags": ["Python"],
         "url": "/baekjoon/baekjoon7/",
         "teaser": null
       }]
