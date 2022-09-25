6f0c5504bb1f488337b620b3a03cf12fd65b8d62
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-25 21:59:35 +0900

    Update 2022-09-20-html1.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,14 +1,8 @@
 var store = [{
-        "title": "[HTML] 1. HTML 기초",
+        "title": "[F/E] 1. HTML",
         "excerpt":"웹 (Web) 월드 와이드 웹 (WWW)이라고도 부름 인터넷을 통해 제공되는 전세계적인 정보 공유 서비스 공간 인터넷 (Internet) 전세계적으로 연결된 컴퓨터 네트워크 공간 수많은 클라이언트, 서버, 이들로 구성된 네트워크의 집합 TCP/IP 통신 규약을 통해 파일 송수신 네트워크 (Network) 컴퓨터들이 통신망을 통해 하나로 연결된 통신 체계 클라이언트 (Client) 네트워크에 연결되어 서버에 정보...","categories": ["Frontend"],
         "tags": ["HTML"],
         "url": "/frontend/html1/",
-        "teaser": null
-      },{
-        "title": "[HTML] 2. HTML 태그 - <head>",
-        "excerpt":"&lt;html&gt; : HTML으로 작성된 문서임을 선언하는 태그 &lt;html&gt; ... &lt;/html&gt; &lt;!DOCTYPE html&gt; : 마크업 언어에서의 문서 형식을 정의하는 태그 DTD 태그 (Document Type Definition) 웹 브라우저의 렌더링 엔진이 관용 모드 (Quirks Mode)가 아닌 표준 모드로 렌더링 HTML5 :&lt;!DOCTYPE html&gt;으로 선언 &lt;!DOCTYPE html&gt; ... &lt;/html&gt; &lt;html lang=\"\"&gt; : 웹 문서의 텍스트가...","categories": ["Frontend"],
-        "tags": ["HTML"],
-        "url": "/frontend/html2/",
         "teaser": null
       },{
         "title": "[BOJ] 1000. A+B (B5)",
