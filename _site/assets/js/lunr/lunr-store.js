0ba04b89cade2b57673a537b6750df3c064b5207
pocj8ur4in <pocj8ur4in@gmail.com>
2022-09-28 16:02:46 +0900

    Create 2022-09-28-baekjoon11.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -94,4 +94,10 @@ var store = [{
         "tags": ["Python"],
         "url": "/baekjoon/baekjoon10/",
         "teaser": null
+      },{
+        "title": "[BOJ] 2525. 오븐 시계 (B3)",
+        "excerpt":"1. 문제 조건 KOI 전자에서는 건강에 좋고 맛있는 훈제오리구이 요리를 간편하게 만드는 인공지능 오븐을 개발하려고 한다. 인공지능 오븐은 오븐구이가 끝나는 시간을 분 단위로 자동적으로 계산한다. 또한, 인공지능 오븐에는 사용자에게 훈제오리구이 요리가 끝나는 시각을 알려 주는 디지털 시계가 있다. 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가...","categories": ["Baekjoon"],
+        "tags": ["Python"],
+        "url": "/baekjoon/baekjoon11/",
+        "teaser": null
       }]
